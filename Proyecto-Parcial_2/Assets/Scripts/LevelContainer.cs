a4b507a19d23fcdf89e75831e0f68f9b9bdd398b
Daniel5123 <156048599+Daniel5123@users.noreply.github.com>
2024-03-13 22:20:49 -0600

    Update LevelContainer.cs

--- Proyecto-Parcial_2/Assets/Scripts/LevelContainer.cs
+++ Proyecto-Parcial_2/Assets/Scripts/LevelContainer.cs
@@ -8,24 +8,26 @@ public class LevelContainer : MonoBehaviour
 {
 
     [Header("GameObject Configuration")]
+    //Aqui estan las variables para las lecciones, cuantas lecciones son y si la leccion esta completa.
     public int Lection = 0;
     public int currentLession = 0;
     public int TotalLession = 0;
     public bool AreAllLessonsComplete = false;
-
+    //Aqui se generan los textos.
     [Header("UI Configuration")]
     public TMP_Text StageTitle;
     public TMP_Text LessonStage;
-
+    //Este es el GameObject que contiene las lecciones.
     [Header("External GameObject Configuration")]
     public GameObject lessonContainer;
-
+    //Este ScriptableObject lee las lecciones.
     [Header("Lesson Data")]
     public ScriptableObject LessonData;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Se actualiza el lessonContainer y se va a OnUpdateUi.
         if (lessonContainer != null)
         {
             OnUpdateUI();
@@ -40,22 +42,25 @@ public class LevelContainer : MonoBehaviour
 
     public void OnUpdateUI()
     {
-
+        // Se accede a los textos de stagetitle y lessonStange 
         if (StageTitle != null || LessonStage != null)
         {
+            //Se muestra en la UI la leccion y su seccion de cada una
             StageTitle.text = "Leccion " + Lection;
             LessonStage.text = "Leccion " + currentLession + " de " + TotalLession;
         }
 
         else
         {
+            //Si no estan los textos se manda un mensaje.
             Debug.LogWarning("GameObject Nulo, revisa las variables de tipo TMP_text");
         }
     }
-
+    //este meotodo activa y desactiva la ventana lessonContainer
     public void EnableWindow()
     {
         OnUpdateUI();
+        //Hace que desaparezco y aparezca la ventana de lessonContainer
         if (lessonContainer.activeSelf)
         {
             //Desactiva el objecto si esta activo
